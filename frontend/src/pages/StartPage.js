3494258b7572dc6c04be7bf304152f447087bf62
sn1p2r78 <117768713+sn1p2r78@users.noreply.github.com>
2025-01-06 18:01:00 +0200

    Enhance StartPage appearance with modern design using Tailwind CSS.

--- frontend/src/pages/StartPage.js
+++ frontend/src/pages/StartPage.js
@@ -2,11 +2,11 @@ import react from 'react';
 
 const StartPage = () => {
     return (
-      <div className="flex flex-col items-center h-screen gap-y-5 backgray-500">
-        <h1 className="text-4x text-white font-bold">Start Page</h1>
-        <p
-className="text-sl t-lg bg-white filter shadow-ld p-4">Welcome to the Image-to-Text conversion service</p>
-	          </div>
+      <div className="flex flex-col justify items-center v-height-screen bg-gradient-to-l from-blue-to-indigo px-5">
+        <h1 className="text-4x text-white font-bold mb-4">CaptchaXtract</h1>
+        <p className="text-lg bg-white bg-opacity-40 p-6 px-4">The Ultimate Image-to-Text Solution for Modern Uses.</p>
+        <button className="bg-white text-black font-medium pt-4 px-6">Start Uploading</button>
+      </div>
     );
 };
 
