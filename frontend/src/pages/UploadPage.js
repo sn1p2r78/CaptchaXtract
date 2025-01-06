376b52789d719da1ca3159223ee3d4a68ea13149
sn1p2r78 <117768713+sn1p2r78@users.noreply.github.com>
2025-01-06 17:58:30 +0200

    Style UploadPage component using Tailwind CSS.

--- frontend/src/pages/UploadPage.js
+++ frontend/src/pages/UploadPage.js
@@ -8,10 +8,10 @@ const UploadPage = () => {
     };
 
     return (
-      <div>
-        <h1>Upload Page</h1>
-        <input type="file" onChange={(handleUpload)} />
-        <p>File Under: {feedback}</p>
+      <div className="flex flex-col items-center bg-blue-200 h-screen gy-5">
+        <h1 className="text-3x text-center text-bold text-transparent mb-2">Upload Your Image</h1>
+        <input type="file" className="block w-full mt-4" onChange={(handleUpload)} />
+        <p className="text-center text-sm mb-4">Selected File: {feedback}</p>
       </div>
     );
 };
