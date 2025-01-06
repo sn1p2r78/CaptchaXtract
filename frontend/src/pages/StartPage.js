da0b2f1e8c57c7215922b8332e136f9478c4bd62
sn1p2r78 <117768713+sn1p2r78@users.noreply.github.com>
2025-01-06 17:56:06 +0200

    Add StartPage component to React frontend.

---
+++ frontend/src/pages/StartPage.js
@@ -0,0 +1,12 @@
+import react from 'react';
+
+const StartPage = () => {
+    return (
+      <div>
+        <h1>Start Page</h1>
+        <p>The basic welcome page.</p>
+      </div>
+    );
+};
+
+export default StartPage;
