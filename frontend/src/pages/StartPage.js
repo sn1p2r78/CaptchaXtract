aff3c4674810e81ace9b82754c470fe89c84dde6
sn1p2r78 <117768713+sn1p2r78@users.noreply.github.com>
2025-01-06 17:58:00 +0200

    Style StartPage component using Tailwind CSS.

--- frontend/src/pages/StartPage.js
+++ frontend/src/pages/StartPage.js
@@ -2,10 +2,11 @@ import react from 'react';
 
 const StartPage = () => {
     return (
-      <div>
-        <h1>Start Page</h1>
-        <p>The basic welcome page.</p>
-      </div>
+      <div className="flex flex-col items-center h-screen gap-y-5 backgray-500">
+        <h1 className="text-4x text-white font-bold">Start Page</h1>
+        <p
+className="text-sl t-lg bg-white filter shadow-ld p-4">Welcome to the Image-to-Text conversion service</p>
+	          </div>
     );
 };
 
